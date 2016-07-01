4cdb1436789e91d022cc97cde0a33cd1bc943d19
Brandon <broach@cern.ch>
2016-07-01 14:15:12 +0200

    changed prestep to midstep (so hits don't appear to have posn inside absorber layers)

--- Detector/DetSensitive/src/EcalCalorimeterSD.cpp
+++ Detector/DetSensitive/src/EcalCalorimeterSD.cpp
@@ -45,7 +45,7 @@ bool EcalCalorimeterSD::ProcessHits(G4Step* aStep, G4TouchableHistory*)
   CLHEP::Hep3Vector prePos = aStep->GetPreStepPoint()->GetPosition();
   CLHEP::Hep3Vector postPos = aStep->GetPostStepPoint()->GetPosition();
   CLHEP::Hep3Vector midPos = 0.5*(postPos + prePos);
-  DD4hep::Simulation::Position pos(prePos.x(), prePos.y(), prePos.z());  
+  DD4hep::Simulation::Position pos(midPos.x(), midPos.y(), midPos.z());  
   // check the cell ID
   uint64_t id = segmentation::cellID(m_seg, *aStep);
   DD4hep::Simulation::Geant4CalorimeterHit* hit, *hitMatch = nullptr;
